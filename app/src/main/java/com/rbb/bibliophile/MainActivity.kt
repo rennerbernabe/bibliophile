ef055dc29087a162c6579f30512f3e82760b71e4
Renner <rennerbrian.bernabe@gmail.com>
2024-03-11 14:52:34 +0800

    Add bottom navigation bar

--- app/src/main/java/com/rbb/bibliophile/MainActivity.kt
+++ app/src/main/java/com/rbb/bibliophile/MainActivity.kt
@@ -1,29 +1,107 @@
 package com.rbb.bibliophile
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.rbb.bibliophile.ui.theme.BibliophileTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val homeTab = TabBarItem(
+                title = stringResource(R.string.home),
+                selectedIcon = Icons.Filled.Home,
+                unselectedIcon = Icons.Outlined.Home,
+            )
+            val favoriteTab = TabBarItem(
+                title = stringResource(R.string.favorite),
+                selectedIcon = Icons.Filled.Favorite,
+                unselectedIcon = Icons.Outlined.FavoriteBorder,
+            )
+            val shelfTab = TabBarItem(
+                title = stringResource(R.string.shelf),
+                selectedIcon = ImageVector.vectorResource(R.drawable.library_books),
+                unselectedIcon = ImageVector.vectorResource(R.drawable.library_books_outline),
+            )
+            val tabBarItems = listOf(homeTab, favoriteTab, shelfTab)
+            val navController = rememberNavController()
+
             BibliophileTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Scaffold(
+                        bottomBar = { TabView(tabBarItems, navController) }
+                    ) {
+                        NavHost(navController = navController, startDestination = homeTab.title) {
+                            composable(homeTab.title) {
+                                Text(homeTab.title)
+                            }
+                            composable(favoriteTab.title) {
+                                Text(favoriteTab.title)
+                            }
+                            composable(shelfTab.title) {
+                                Text(shelfTab.title)
+                            }
+                        }
+                    }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun TabView(tabBarItems: List<TabBarItem>, navController: NavHostController) {
+    var selectedTabIndex by rememberSaveable { mutableIntStateOf(0) }
+
+    NavigationBar {
+        tabBarItems.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = selectedTabIndex == index,
+                onClick = {
+                    selectedTabIndex = index
+                    navController.navigate(item.title)
+                },
+                icon = {
+                    Icon(
+                        imageVector = if (index == selectedTabIndex) {
+                            item.selectedIcon
+                        } else item.unselectedIcon,
+                        contentDescription = item.title
+                    )
+                }
+            )
         }
     }
 }
---
+++ app/src/main/java/com/rbb/bibliophile/TabBarItem.kt
@@ -0,0 +1,9 @@
+package com.rbb.bibliophile
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class TabBarItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
